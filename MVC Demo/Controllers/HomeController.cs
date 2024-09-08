1a7f8212d51f1dc6b39c9d00183cb183d90c30a2
Elsayed Ahmed <elsayedahmed.official1@gmail.com>
2024-09-08 15:28:09 +0300

    CreationOfSession01

---
+++ MVC Demo/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ContentResult result = new ContentResult();
+            //result.Content = "Hello From Content Result";
+            //return result;
+
+            return Content("Hello From Content Result");
+        }
+        public string AboutUs()
+        {
+            return "This is Aboutus Action ";
+        }
+    }
+}
